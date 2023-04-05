ef5817f1ae314b0c4f4d015ff4462c900ddced7d
Geogz <francogutierrez121@gmail.com>
2023-04-05 16:42:56 -0500

    ultimo commit esta vez si

--- conexion.php
+++ conexion.php
@@ -1,11 +1,15 @@
 <?php
+
 	function conexion(){
-	$host = "host=containers-us-west-142.railway.app";
-	$port = "port=6866";
+
+	$host = "host=containers-us-west-40.railway.app";
+	$port = "port=5965";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=ycdzUNfIO6sYi9DFyFCK";
+	$password = "password=ImXWTfnoVNeO4Av9eXLI";
+	
 	$db = pg_connect("$host $port $dbname $user $password");
+
     return $db;
 }
 ?>
