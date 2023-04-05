4198e71b9eedfe3efed4d09b9372abdec782865a
Geogz <francogutierrez121@gmail.com>
2023-04-05 16:20:40 -0500

    datos ingresar

--- index-post.php
+++ index-post.php
@@ -2,11 +2,11 @@
 include("conexion.php");
 $con = conexion();
 
-$doc = $_POST["doc"];
-$nom = $_POST["nom"];
-$ape = $_POST["ape"];
-$dir = $_POST["dir"];
-$cel = $_POST["cel"];
+$doc = $_POST["documento"];
+$nom = $_POST["nombre"];
+$ape = $_POST["apellido"];
+$dir = $_POST["direccion"];
+$cel = $_POST["celular"];
 
 $sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
 pg_query($con, $sql);
