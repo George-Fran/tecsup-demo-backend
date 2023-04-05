c6921d6f2e90a420d0c5ef2abc5c5fffa0462108
Geogz <francogutierrez121@gmail.com>
2023-04-05 16:02:53 -0500

    conectar bd

--- conexion.php
+++ conexion.php
@@ -3,6 +3,7 @@
 	$host = "host=containers-us-west-142.railway.app";
 	$port = "port=6866";
 	$dbname = "dbname=railway";
+	$pepe = "pepe";
 	$user = "user=postgres";
 	$password = "password=ycdzUNfIO6sYi9DFyFCK";
 	$db = pg_connect("$host $port $dbname $user $password");
