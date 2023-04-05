bfef6f0620f80ee42ed643460a1552f44590314c
Geogz <francogutierrez121@gmail.com>
2023-04-05 15:52:49 -0500

    conexiones

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-38.railway.app";
-	$port = "port=8029";
+	$host = "host=containers-us-west-142.railway.app";
+	$port = "port=6866";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=JIcL9swq3Y5O3TS911yU";
+	$password = "password=ycdzUNfIO6sYi9DFyFCK";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
